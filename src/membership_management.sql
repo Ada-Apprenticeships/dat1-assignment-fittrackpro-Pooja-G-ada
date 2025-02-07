3d8fcc7079058e7e770f6a9b99d26a836b35a8a2
Pooja Gurung <pooja.gurung@ada.ac.uk>
2025-02-07 17:42:27 +0000

    membership management done

--- src/membership_management.sql
+++ src/membership_management.sql
@@ -9,9 +9,37 @@ PRAGMA foreign_keys = ON;
 
 -- 1. List all active memberships
 -- TODO: Write a query to list all active memberships
+SELECT
+    ms.member_id,
+    m.first_name,
+    m.last_name,
+    type AS membership_type,
+    m.join_date,
+    status
+FROM memberships ms
+JOIN members m ON ms.member_id = m.member_id
+WHERE status LIKE 'Active';
 
 -- 2. Calculate the average duration of gym visits for each membership type
 -- TODO: Write a query to calculate the average duration of gym visits for each membership type
+SELECT 
+    ms.type AS membership_type,
+    --use AVG() function to calculate average after grouping by type, CAST/convert into integer to get whole no.
+    CAST (AVG(strftime('%s', check_out_time) - strftime('%s', check_in_time)) / 60 AS INTEGER) AS avg_visit_duration_minutes -- avg_visit_duration_minutes
+FROM attendance a
+JOIN members m ON a.member_id = m.member_id
+JOIN memberships ms ON a.member_id = ms.member_id
+GROUP BY membership_type;
 
 -- 3. Identify members with expiring memberships this year
 -- TODO: Write a query to identify members with expiring memberships this year
+
+SELECT 
+    ms.member_id,
+    m.first_name,
+    m.last_name,
+    m.email,
+    ms.end_date
+FROM memberships ms
+JOIN members m ON ms.member_id = m.member_id
+WHERE ms.end_date <= '2025-12-31';
